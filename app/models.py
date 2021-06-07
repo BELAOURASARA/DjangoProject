33d87d0d4f542b0f0700979ec33b3b8c4d96a1d0
BELAOURASARA <hs_belaoura@esi.dz>
2021-06-07 18:56:59 +0100

    second commit

--- app/models.py
+++ app/models.py
@@ -3,11 +3,20 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class SujetThese(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    #date_posted = models.DateTimeField(default=timezone.now)
+    enseignant = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class Affect(models.Model):
+    sujet = models.ForeignKey(SujetThese, on_delete=models.CASCADE)
+    laureat =  models.ForeignKey(User, on_delete=models.CASCADE)
+   
+   
+
+   
--- myapp/settings.py
+++ myapp/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Templates Directory
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 
 # Application definition
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
